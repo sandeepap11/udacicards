28b86b1b7e81a1e99e4881f912a3ec5511f556e2
Sandeep Madavu Ramachandra <v-sander@microsoft.com>
2018-05-11 16:34:19 +0530

    feat: remove tab navigation

--- App.js
+++ App.js
@@ -7,48 +7,11 @@ import ListView from './components/ListView';
 import Dummy from './components/Dummy';
 
 
-
-const Tabs = TabNavigator({
-  ListView: {
-    screen: ListView,
-    navigationOptions: {
-      tabBarLabel: 'Decks',
-      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
-    },
-  },
-  Dummy: {
-    screen: Dummy,
-    navigationOptions: {
-      tabBarLabel: 'Dummy',
-      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
-    },
-  }
-}, {
-  navigationOptions: {
-    header: null
-  },
-  tabBarOptions: {
-    activeTintColor: Platform.OS === 'ios' ? "purple" : "white",
-    style: {
-      height: 56,
-      backgroundColor: Platform.OS === 'ios' ? "white" : "purple",
-      shadowColor: 'rgba(0, 0, 0, 0.24)',
-      shadowOffset: {
-        width: 0,
-        height: 3
-      },
-      shadowRadius: 6,
-      shadowOpacity: 1
-    }
-  }
-})
-
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-      <ListView/>
-        
+      <ListView/>        
       </View>
     );
   }
