02f4f3981f353bb1fee92b17f74cd4e2049d9a28
sandeepramachandra <sandeepap11@gmail.com>
2018-05-14 22:59:33 +0530

    feat: add data

--- App.js
+++ App.js
@@ -9,13 +9,25 @@ import Deck from './components/Deck';
 
 const decks = [
   {id:"d1", name: "udacicards", cards:["c1", "c2", "c3", "c4", "c13"]},
-  {id:"d2", name: "Card Deck 1", cards:["c5", "c9", "c11", "c14"]},
-  {id:"d3", name: "Lifestyle Choices", cards:["c6", "c7", "c8", "c10", "c12"]}
+  {id:"d2", name: "Capitals", cards:["c5", "c9", "c11", "c12", "c14"]},
+  {id:"d3", name: "Lifestyle Choices", cards:["c6", "c7", "c8", "c10"]}
 ];
 
-const cards = [{id: "c1"}, {id: "c1"}, {id: "c1"}, {id: "c1"}, {id: "c1"}, {id: "c1"}, {id: "c1"}, {id: "c1"},
-  {id: "c1"}, {id: "c1"}, {id: "c1"}, {id: "c1"}, {id: "c1"}
-];
+const cards = [ {id: "c1", question: "Is react fine?", answer: "Yes"}, 
+                {id: "c2", question: "Are you fine?", answer: "No"},
+                {id: "c3", question: "Is Tyler good?", answer: "Yes"}, 
+                {id: "c4", question: "Is Richard good?", answer: "Lol"}, 
+                {id: "c5", question: "Karnataka", answer: "Bengaluru"}, 
+                {id: "c6", question: "Coloring or no", answer: "Hmm"},
+                {id: "c7", question: "Is Behrouz Biriyani the best?", answer: "Ain't that bad"}, 
+                {id: "c8", question: "Who's the boss?", answer: "A girl"}, 
+                {id: "c9", question: "California", answer: "Sacramento"}, 
+                {id: "c10", question: "What's the best beer?", answer: "Root beer"}, 
+                {id: "c11", question: "Washington", answer: "Olympia"}, 
+                {id: "c12", question: "New Zealand", answer: "Wellington"}, 
+                {id: "c13", question: "Should state be mutated?", answer: "No"}, 
+                {id: "c14", question: "Lebanon", answer: "Beirut"}
+              ];
 
 export default class App extends Component {
   render() {
--- components/ListView.js
+++ components/ListView.js
@@ -1,39 +1,39 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View,StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Deck from './Deck';
 
 
 
 export default class ListView extends Component {
-    
+
 
     render() {
 
-        const {decks} = this.props;
-        
-      return (
-        <View style={styles.container}>
-        {
-            decks.map((deck) => (
-                <Deck key={deck.id} deck={deck} view="list"/>
-            ))
-        }
-        </View>
-      );
+        const { decks } = this.props;
+
+        return (
+            <View style={styles.container}>
+                {
+                    decks.map((deck) => (
+                        <Deck key={deck.id} deck={deck} view="list" />
+                    ))
+                }
+            </View>
+        );
     }
-  }
-  
-  const styles = StyleSheet.create({
+}
+
+const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center'
-  
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+
     },
     deck: {
-        backgroundColor: 'purple',        
+        backgroundColor: 'purple',
         height: 200,
         width: 250,
     }
-  });
+});
